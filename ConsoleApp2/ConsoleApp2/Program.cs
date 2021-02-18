6c2e737b2a27f4d75e78a80a04002c2a0118c626
Walid Marghata <walid.marghata@montreal.com.br>
2021-02-18 12:59:01 -0300

    commit do projeto da calculadora

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Processar();
+
+            Console.ReadKey();
+        }
+
+        private static void Processar()
+        {
+            int GALO;
+            Console.Write("Informe a seu time: ");
+            int.TryParse(Console.ReadLine(), out GALO);
+
+            if (GALO >= 13)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("ATLETICANO");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("NAO ATLETICANO");
+            }
+        }
+    }
+}
